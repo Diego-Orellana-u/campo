29bdc51874d778a6a4623dec1847f3e39e12a1d6
diego orellana <diego.orellana.git@gmail.com>
2025-01-12 16:39:11 -0300

    finished menu model and added initial serializer

--- campoCore/menu/models.py
+++ campoCore/menu/models.py
@@ -5,3 +5,5 @@ class Menu(models.Model):
   name = models.CharField(max_length=100)
   price = models.DecimalField()
   description = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
